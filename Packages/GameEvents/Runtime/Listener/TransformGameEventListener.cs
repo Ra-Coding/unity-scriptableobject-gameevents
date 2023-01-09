753c986f4bbb04a5a9e96d2b6771bec94a564dcd
Fireball19 <fireball1199@googlemail.com>
2023-01-09 21:33:19 +0100

    Add transform gameevent and listener

---
+++ Packages/GameEvents/Runtime/Listener/TransformGameEventListener.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace RaCoding.GameEvents
+{
+    [System.Serializable]
+    public class TransformGameEventListener : GameEventListener<Transform> 
+    {
+        public TransformGameEventListener() : base() {}
+        
+        public TransformGameEventListener(IRegisterListener registerListener)  : base(registerListener) {}
+
+        [SerializeField] private TransformGameEvent transformEvent;
+        [SerializeField] private TransformUnityEvent transformResponse;
+
+        public override GameEvent<Transform> Event => transformEvent;
+        public override UnityEvent<Transform> Response => transformResponse;
+    }
+}
