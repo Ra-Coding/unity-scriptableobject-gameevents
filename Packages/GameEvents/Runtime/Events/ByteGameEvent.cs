388e214a5bf830e5891713dd0da08b7e98c88144
Fireball19 <fireball1199@googlemail.com>
2023-01-09 21:31:49 +0100

    Add byte gameevent and listener

---
+++ Packages/GameEvents/Runtime/Events/ByteGameEvent.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace RaCoding.GameEvents 
+{
+    [CreateAssetMenu(fileName = "ByteGameEvent", menuName = "RaCoding/GameEvent/Create new byte game event")]
+    public class ByteGameEvent : GameEvent<byte> {}
+}
