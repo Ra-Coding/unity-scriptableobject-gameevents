dcede0ddb795d8ea65594525d3e2f009afe210f9
Fireball19 <fireball1199@googlemail.com>
2023-01-09 21:32:05 +0100

    Add char gameevent and listener

---
+++ Packages/GameEvents/Runtime/Listener/CharGameEventListener.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace RaCoding.GameEvents
+{
+    [System.Serializable]
+    public class CharGameEventListener : GameEventListener<char> 
+    {
+        public CharGameEventListener() : base() {}
+        
+        public CharGameEventListener(IRegisterListener registerListener)  : base(registerListener) {}
+
+        [SerializeField] private CharGameEvent charEvent;
+        [SerializeField] private CharUnityEvent charResponse;
+
+        public override GameEvent<char> Event => charEvent;
+        public override UnityEvent<char> Response => charResponse;
+    }
+}
