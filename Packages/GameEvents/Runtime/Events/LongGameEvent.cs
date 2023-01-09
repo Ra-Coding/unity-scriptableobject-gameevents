1d15194dcde7382df571fb3c20cf3802adf73b71
Fireball19 <fireball1199@googlemail.com>
2023-01-09 21:32:49 +0100

    Add long gameevent and listener

---
+++ Packages/GameEvents/Runtime/Events/LongGameEvent.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace RaCoding.GameEvents 
+{
+    [CreateAssetMenu(fileName = "LongGameEvent", menuName = "RaCoding/GameEvent/Create new long game event")]
+    public class LongGameEvent : GameEvent<long> {}
+}
---
+++ Packages/GameEvents/Runtime/Listener/LongGameEventListener.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace RaCoding.GameEvents
+{
+    [System.Serializable]
+    public class LongGameEventListener : GameEventListener<long> 
+    {
+        public LongGameEventListener() : base() {}
+        
+        public LongGameEventListener(IRegisterListener registerListener)  : base(registerListener) {}
+
+        [SerializeField] private LongGameEvent longEvent;
+        [SerializeField] private LongUnityEvent longResponse;
+
+        public override GameEvent<long> Event => longEvent;
+        public override UnityEvent<long> Response => longResponse;
+    }
+}
