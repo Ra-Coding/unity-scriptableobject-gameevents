f4b7cfa26a9fc70b89e8bdaec84fd94905c2474f
Fireball19 <fireball1199@googlemail.com>
2023-01-09 21:32:25 +0100

    Add color gameevent and listener

---
+++ Packages/GameEvents/Runtime/Events/ColorGameEvent.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace RaCoding.GameEvents 
+{
+    [CreateAssetMenu(fileName = "ColorGameEvent", menuName = "RaCoding/GameEvent/Create new color game event")]
+    public class ColorGameEvent : GameEvent<Color> {}
+}
