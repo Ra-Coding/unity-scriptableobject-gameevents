f412bd926194fa942bc7db5ba4d86552ffcc87c5
Fireball19 <fireball1199@googlemail.com>
2023-01-09 21:33:03 +0100

    Add short gameevent and listener

---
+++ Packages/GameEvents/Runtime/Events/ShortGameEvent.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace RaCoding.GameEvents 
+{
+    [CreateAssetMenu(fileName = "ShortGameEvent", menuName = "RaCoding/GameEvent/Create new short game event")]
+    public class ShortGameEvent : GameEvent<short> {}
+}
---
+++ Packages/GameEvents/Runtime/Listener/Component/ShortComponentGameEventListener.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace RaCoding.GameEvents
+{
+    [AddComponentMenu("RaCoding/GameEvents/ShortGameEventListener")]
+    public class ShortComponentGameEventListener : ComponentGameEventListener<ShortGameEventListener, short> {}
+}
