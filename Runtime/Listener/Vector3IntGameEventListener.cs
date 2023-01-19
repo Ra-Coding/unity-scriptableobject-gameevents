02905d25337312f5eba0b4a402c761a819240765
Fireball19 <fireball1199@googlemail.com>
2023-01-19 19:17:43 +0100

    Add vector3int gameevent

---
+++ Runtime/Listener/Vector3IntGameEventListener.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace RaCoding.GameEvents
+{
+    [System.Serializable]
+    public class Vector3IntGameEventListener : GameEventListener<Vector3Int>
+    {
+        public Vector3IntGameEventListener() : base() { }
+
+        public Vector3IntGameEventListener(IRegisterListener registerListener) : base(registerListener) { }
+
+        [SerializeField] private Vector3IntGameEvent vector3intEvent;
+        [SerializeField] private Vector3IntUnityEvent vector3intResponse;
+
+        public override GameEvent<Vector3Int> Event => vector3intEvent;
+        public override UnityEvent<Vector3Int> Response => vector3intResponse;
+    }
+}
