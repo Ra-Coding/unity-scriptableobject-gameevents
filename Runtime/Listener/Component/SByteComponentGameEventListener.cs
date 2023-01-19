8e5b2807abbc982536f59c0c9c25e4c0761263df
Fireball19 <fireball1199@googlemail.com>
2023-01-19 18:46:25 +0100

    Add sbyte gameevent

---
+++ Runtime/Listener/Component/SByteComponentGameEventListener.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace RaCoding.GameEvents
+{
+    [AddComponentMenu("RaCoding/GameEvents/SByteGameEventListener")]
+    public class SByteComponentGameEventListener : ComponentGameEventListener<SByteGameEventListener, sbyte> { }
+}
