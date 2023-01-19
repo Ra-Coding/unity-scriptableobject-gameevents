e9d496416d0d52dd9202eb6e85df6232af7f6808
Fireball19 <fireball1199@googlemail.com>
2023-01-19 19:17:43 +0100

    Add vector3int gameevent

---
+++ Packages/SOGameEvents/Runtime/Events/Vector3IntGameEvent.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace RaCoding.GameEvents
+{
+    [CreateAssetMenu(fileName = "Vector3IntGameEvent", menuName = "RaCoding/GameEvent/Create new vector3int game event")]
+    public class Vector3IntGameEvent : GameEvent<Vector3Int> { }
+}
