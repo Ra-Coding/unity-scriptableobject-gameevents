fd65e72b0e783fd36b6e708b804d72d34372e831
Fireball19 <fireball1199@googlemail.com>
2023-01-19 19:39:32 +0100

    Add missing byte component gameeventlistener code

---
+++ Packages/SOGameEvents/Runtime/Listener/Component/ByteComponentGameEventListener.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace RaCoding.GameEvents
+{
+    [AddComponentMenu("RaCoding/GameEvents/ByteGameEventListener")]
+    public class ByteComponentGameEventListener : ComponentGameEventListener<ByteGameEventListener, byte> { }
+}
